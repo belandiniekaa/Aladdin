507a44e7697b74fa0a890f828dd47a05b36e111f
regtat <regitaghani@gmail.com>
2023-11-26 09:53:29 +0700

    session pada permintaan

---
+++ games/permintaan1.php
@@ -0,0 +1,132 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Aladin&family=Alata&display=swap" rel="stylesheet">
+    <style>
+        .navbar{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            box-align: center;
+            width: 100%;
+            height: 70px;
+            background-color: #0c133f;
+            font-weight: 900;
+            font-size: 25px;
+        }
+
+        .isinavbar{
+            color: white;
+            display: flex;
+            padding: 0px 15px 0px 15px;
+        }
+
+
+        @media only screen and (max-width: 900px){
+            .navbar{
+                width:100%;
+            }
+        }
+
+        body{
+            background-image: url('../img/Empty-Backdrop-from-Aladdin-disney-crossover-29212922-1099-648.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 0;
+            font-family: alata;
+        }
+
+        img{
+            width: 60%;
+        }
+        .lampu{
+            position: fixed;
+            bottom: 10%;
+            right: 0%;
+        }
+
+        .aladin{
+            position: fixed;
+            bottom: 2%;
+            left: 15%;
+        }
+
+        .lampu1{
+            position: fixed;
+            bottom: 52%;
+            right: 3%;
+        }
+
+        .aladin1{
+            position: fixed;
+            bottom: 48%;
+            left: 17%;
+        }
+        
+        span{
+            color: #0c133f;
+            font-size: 15px;
+            font-weight: 600;
+        }
+        .text1{
+            position: fixed;
+            bottom: 74%;
+            right: 24%;
+            text-align: justify;
+        }
+
+        a{
+            color: #1b2da2;
+            text-decoration: none;
+        }
+
+        .text2{
+            position: fixed;
+            bottom: 74%;
+            left: 23%;
+        }
+    </style>
+    <title>Permintaan 1</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="isinavbar">
+            <h3>Home</h3>
+        </div>
+        <div class="isinavbar">
+            <h3>Story</h3>
+        </div>
+        <div class="isinavbar">
+            <h3>About Us</h3>
+        </div>
+    </div>
+    <div>
+        <span class="aladin"><img src="../img/Group 11.png" alt=""></span>
+        <span class="lampu"><img src="../img/Group 12.png" alt=""></span>
+        <span class="lampu1"><img src="../img/Group 1.png" alt="">
+            <span class="text1">I'm right here for <br> your wish fulfillment.  <br>Three wishes, to be exact.  <br>What's your first wish?</span>
+        </span>
+        <span class="aladin1"><img src="../img/Group 2.png" alt="">
+            <span class="text2">I want to get out of <a href="permintaan1-1.php">here.</a></span>
+        </span>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+        <br><br><br><br><br><br>
+    </div>
+</body>
+</html>
---
+++ games/permintaan3-no.php
@@ -0,0 +1,225 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Aladin&family=Alata&display=swap" rel="stylesheet">
+    <style>
+        .navbar{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            box-align: center;
+            width: 100%;
+            height: 70px;
+            background-color: #0c133f;
+            font-weight: 900;
+            font-size: 25px;
+        }
+
+        .isinavbar {
+            color: white;
+            display: flex;
+            padding: 0px 15px 0px 15px;
+        }
+
+
+        @media only screen and (max-width: 900px){
+            .navbar{
+                width:100%;
+            }
+        }
+
+        body{
+            background-image: url('../img/luargua.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            display: flex;
+            flex-direction: column;
+            margin: 0;
+            padding: 0;
+            font-family: alata;
+        }
+
+        table{
+            display: flex;
+            box-align: center;
+            text-align: center;
+            justify-content: center;
+        }
+
+        .kotakpilihan1{
+            display: flex;
+            flex-direction: row;
+            margin: 30px;
+            align-items: center;
+            justify-content: center;
+            width: 300px;
+            height: 150px;
+            background-color: #f4a76391;
+            border-radius: 20px;
+            position: relative;
+            transform-style: preserve-3d;
+            transition: transform 0.5s;
+            cursor: pointer;
+        }
+
+        img{
+            width: 300px;
+            height: 150px;
+            border-radius: 20px;
+        }
+
+        .kotakpilihan1.flipped{
+            transform: rotateY(180deg);
+        }
+
+        .depan{
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            backface-visibility: hidden;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 20px;
+            background-color: #f4a76337;
+        }
+
+        .belakang{
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            backface-visibility: hidden;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 20px;
+            background-color: #3498db;
+            transform: rotateY(180deg);
+        }
+
+
+        h1{
+            font-family: aladin;
+            text-align: center;
+        }
+        .judul{
+            font-family: aladin;
+            text-align: center;
+            font-size: 35px;
+        }
+
+        p{
+            text-align: center;
+        }
+
+        @media only screen and (max-width: 900px){
+            body, .kotakpilihan1, .kotakpilihan1.kotakpilihan1.flipped, .depan, .belakang, h1, p, img, table{
+                width:100%;
+            }
+        }
+    </style>
+<script>
+    let selectedKotakpilihan1 = null;
+
+    function flipkotakpilihan1(kotakpilihan1) {
+        if (selectedKotakpilihan1 === null) {
+            if (!kotakpilihan1.classList.contains('flipped')) {
+                kotakpilihan1.classList.add('flipped');
+
+                selectedKotakpilihan1 = kotakpilihan1;
+
+                setTimeout(function(){
+                    window.location.href = 'finish.php';
+                }, 5000);
+            }
+        }
+    }
+</script>
+    
+    
+    <title>Pilihan</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="isinavbar">
+            <h3>Home</h3>
+        </div>
+        <div class="isinavbar">
+            <h3>Story</h3>
+        </div>
+        <div class="isinavbar">
+            <h3>About Us</h3>
+        </div>
+    </div>
+    <div>
+        <br>
+        <h1 class="judul">Choose your last wish!</h1>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <td><div class="kotakpilihan1" onclick="flipkotakpilihan1(this)">
+                    <div class="depan">
+                        <h1>Power</h1>
+                    </div>
+                    <div class="belakang">
+                        <img src="../img/aladdin.jpg" alt="">
+                    </div>
+                </div></td>
+                <td><div class="kotakpilihan1" onclick="flipkotakpilihan1(this)">
+                    <div class="depan">
+                        <h1>Power</h1>
+                    </div>
+                    <div class="belakang">
+                        <img src="../img/aladdin.jpg" alt="">
+                    </div>
+                </div></td>
+                <td><div class="kotakpilihan1" onclick="flipkotakpilihan1(this)">
+                    <div class="depan">
+                        <h1>Power</h1>
+                    </div>
+                    <div class="belakang">
+                        <img src="../img/aladdin.jpg" alt="">
+                    </div>
+                </div></td>
+            </tr>
+            <tr>
+                <td><div class="kotakpilihan1" onclick="flipkotakpilihan1(this)">
+                    <div class="depan">
+                        <h1>Power</h1>
+                    </div>
+                    <div class="belakang">
+                        <img src="../img/aladdin.jpg" alt="">
+                    </div>
+                </div></td>
+                <td><div class="kotakpilihan1" onclick="flipkotakpilihan1(this)">
+                    <div class="depan">
+                        <h1>Power</h1>
+                    </div>
+                    <div class="belakang">
+                        <img src="../img/aladdin.jpg" alt="">
+                    </div>
+                </div></td>
+                <td><div class="kotakpilihan1" onclick="flipkotakpilihan1(this)">
+                    <div class="depan">
+                        <h1>Power</h1>
+                    </div>
+                    <div class="belakang">
+                        <img src="../img/aladdin.jpg" alt="">
+                    </div>
+                </div></td>
+            </tr>
+            
+        </table>
+    </div>
+</body>
+</html>
